c04441df3d96e104e8eb315e9587cd58234535d7
jpike <jacob@jacobpike.com>
2018-09-08 17:26:41 -0400

    Adding space before include path.

--- build_library.sh
+++ build_library.sh
@@ -4,4 +4,4 @@ mkdir -p build
 # BUILD THE LIBRARY.
 COMPILATION_FILE="noah_ark_library.project"
 MAIN_CODE_DIR="code/library"
-$CXX -x c++ $COMPILATION_FILE /I$MAIN_CODE_DIR -o "build/noah_ark_library.lib"
+$CXX -x c++ $COMPILATION_FILE /I $MAIN_CODE_DIR -o "build/noah_ark_library.lib"

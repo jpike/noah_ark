1b1350df89df3eb943a2f2068321704264ca5e79
jpike <jacob@jacobpike.com>
2018-09-08 17:17:39 -0400

    Specifying explicit file extension for building library.

--- build_library.sh
+++ build_library.sh
@@ -1,9 +1,6 @@
 # CREATE THE BUILD DIRECTORY IF IT DOESN'T EXIST.
 mkdir -p build
 
-
 # BUILD THE LIBRARY.
 COMPILATION_FILE="noah_ark_library.project"
-$CXX $COMPILATION_FILE -o "build/noah_ark_library.lib"
-
-echo "Done (library)"
+$CXX -x c++ $COMPILATION_FILE -o "build/noah_ark_library.lib"

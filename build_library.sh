2fdcdcc024f18a2604e61a1757737e2c7533b843
jpike <jacob@jacobpike.com>
2018-09-08 19:23:48 -0400

    Adding more debug printing to Travis CI build script.

--- build_library.sh
+++ build_library.sh
@@ -1,6 +1,17 @@
 # CREATE THE BUILD DIRECTORY IF IT DOESN'T EXIST.
 mkdir -p build
 
+# PRINT OUT SOME DEBUG STUFF.
+echo $CXX
+type clang++
+which clang++
+type clang++-7
+which clang++-7
+type g++
+which g++
+type g++-8
+which g++-8
+
 # BUILD THE LIBRARY.
 COMPILATION_FILE="noah_ark_library.project"
 MAIN_CODE_DIR="code/library"
